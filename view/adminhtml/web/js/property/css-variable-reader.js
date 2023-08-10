0f6c82f50487d3a9bd5fec0dc454a759748bcd39
denistrator <denistrator@yandex.com>
2023-08-10 12:40:13 +0300

    SRP-220: implement PB Columns grid

---
+++ view/adminhtml/web/js/property/css-variable-reader.js
@@ -0,0 +1,46 @@
+/*eslint-disable */
+/* jscs:disable */
+define(["Magento_PageBuilder/js/utils/string"], function (_string) {
+  /**
+   * Copyright © Magento, Inc. All rights reserved.
+   * See COPYING.txt for license details.
+   */
+
+  /**
+   * @api
+   */
+  var StylePropertyReader = /*#__PURE__*/function () {
+    "use strict";
+
+    function StylePropertyReader() {}
+
+    var _proto = StylePropertyReader.prototype;
+
+    /**
+     * Read style property from element
+     *
+     * @param {HTMLElement} element
+     * @param {string} source
+     * @returns {string | object}
+     */
+    _proto.read = function read(element, source) {
+        let cssVarName = source.replace('css_var_', '').replaceAll('_', '-');
+        let elemStyles = element.getAttribute('data-desktop-style').split(';');
+        let targetStyleLine = elemStyles.filter(function name(elem) {
+            return elem.includes(cssVarName);
+        });
+
+        if (targetStyleLine.length === 0) {
+            return '';
+        }
+
+        let targetStyle = targetStyleLine[0].split(':');
+
+        return targetStyle[1].trim();
+    };
+
+    return StylePropertyReader;
+  }();
+
+  return StylePropertyReader;
+});
